9c2c1802d698880702472f25f860a7d5dbf70ae7
xiaoyongyuan <2207294253@qq.com>
2019-05-08 15:56:54 +0800

    登录背景效果

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './index.less';
-
+import "../../style/ztt/css/fastspeed.css";
+import "../../style/ztt/css/style.css";
+import "../../style/ztt/css/normalize.css"
 class Login extends Component {
   render() {
     return (
       <div className="Login">
-        Login
+          <div id="stars"></div>
+          <div id="stars2"></div>
+          <div id="stars3"></div>
+          <div className="m-fastspeed-banner"></div>
       </div>
     );
   }
