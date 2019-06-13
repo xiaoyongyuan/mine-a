57a282b13311b2dd447f4a4322f159d1291bb17e
mikeJang <oncwnuN6uHaFk5pVuYtZaUze_3tk@git.weixin.qq.com>
2019-06-13 09:36:47 +0800

    jhy

---
+++ src/components/monitor/AlarmInfo.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+class AlarmInfo extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>AlarmInfo</div>;
+  }
+}
+export default AlarmInfo;
---
+++ src/components/monitor/CurveChart.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+class CurveChart extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>CurveChart</div>;
+  }
+}
+export default CurveChart;
