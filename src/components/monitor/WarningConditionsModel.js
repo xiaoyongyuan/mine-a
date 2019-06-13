01ac74aaf0fd7f87eaecabd0c3a0ea5271ed4db4
xiaoyongyuan <2207294253@qq.com>
2019-06-13 17:10:42 +0800

    预警

--- src/components/monitor/WarningConditionsModel.js
+++ src/components/monitor/WarningConditionsModel.js
@@ -1,16 +1,86 @@
 import React, { Component } from 'react';
 import {Form} from "antd";
-import {Modal, Input, Select, Button} from 'antd';
+import {Modal, Cascader, Select, Button} from 'antd';
+import axios from "../../axios";
 const FormItem = Form.Item;
 const Option = Select.Option;
+let vis=false;
 class WarningConditionsModel extends Component{
     constructor(props){
         super(props);
-        this.state={};
+        this.state={
+            options:[]
+        };
     }
     reset = ()=>{ //取消表单
         this.props.form.resetFields();
         this.props.earlyreset()
+    };
+    componentDidMount() {
+        this.network();
+    }
+    //网
+    network=()=>{
+        if(this.props.selectp){
+            axios.ajax({
+                baseURL:window.g.easyURL,
+                method: 'get',
+                url: '/montinet',
+                data:{
+                    code:this.props.selectp
+                }
+            }).then((res)=>{
+                if(res.success){
+                    var arrs=[];
+                    res.data.map((v)=>{
+                        arrs.push({
+                            value: v.code,
+                            label: v.name,
+                        });
+                    });
+                    this.setState({
+                        options:arrs
+                    })
+                }
+            });
+        }
+    };
+    handleFilterSubmit=()=>{
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                this.props.form.resetFields();
+            }
+        });
+    };
+    onChange = (value) => {
+      if(this.props.selectp && value){
+          axios.ajax({
+              baseURL:window.g.easyURL,
+              method: 'get',
+              url: '/monitordot',
+              data:{
+                  projected:this.props.selectp,
+                  netid:value
+              }
+          }).then((res)=>{
+              if(res.success){
+                  console.log(res.data,"888");
+                  res.data.map((v)=>{
+                      this.state.options.push({
+                          children:[{
+                              value: v.code,
+                              label: v.name,
+                          }]});
+                  });
+
+              }
+          });
+      }
+    };
+    loadData = selectedOptions => {
+        const targetOption = selectedOptions[selectedOptions.length - 1];
+        targetOption.loading = true;
+
     };
     render() {
         const { getFieldDecorator } = this.props.form;
@@ -40,7 +110,7 @@ class WarningConditionsModel extends Component{
                                     message: '请输入标题',
                                 }],
                             })(
-                                <Input key='montInput' />
+                                <Cascader options={this.state.options} loadData={this.loadData} onChange={this.onChange} placeholder="请选择监测点" />
                             )
                         }
                     </FormItem>
@@ -96,6 +166,10 @@ class WarningConditionsModel extends Component{
                                 </Select>
                             )
                         }
+                    </FormItem>
+                    <FormItem key="buts">
+                        <Button type='primary' onClick={this.handleFilterSubmit}>确定</Button>
+                        <Button style={{ margin: '0 10px' }} onClick={this.reset}>取消</Button>
                     </FormItem>
                 </Form>
             </Modal>
