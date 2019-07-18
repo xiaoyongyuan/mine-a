d052107a6cbbd12e302c878722f84e1b284e2651
yangalong <172778564@qq.com>
2019-07-18 16:20:38 +0800

    测试

---
+++ src/components/systemmanage/test.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+    constructor(props){
+        super(props);
+        this.state={   
+        };
+    }
+
+    componentDidMount(){
+        
+    }
+
+
+    render() {
+        
+        return (
+            <div className="Test">
+                <div>
+                    基础数据111
+                </div>
+            </div>
+        );
+    }
+}
+export default Test
