24a2cc7b435aaae227bd6fbb75faa7812b993163
wuxiaona123 <137987515@qq.com>
2019-08-13 15:02:01 +0800

    完成图层管理, 矿区导航echarts

---
+++ src/components/common/LayerEcharts1.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import ReactEcharts from 'echarts-for-react';
+import echarts from 'echarts';
+import 'echarts-liquidfill';
+import RemoteEchartgd from '../../style/imgs/RemoteEchartgd.png'
+
+export default class LayerEcharts1 extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            option:{}
+        };
+    }
+    componentWillMount(){
+        // 道路
+        var tilte=this.props.data.tilte;
+        // 百分比
+        var value = this.props.data.value;
+        // 总共
+        var all =this.props.data.all;
+        // 数据
+        var data = [value,value,value,value];
+        // 第二种圆圈样式
+        var two=this.props.two;
+
+        var option = {
+            backgroundColor: 'rgba(0,0,0,0)',
+            graphic: [{
+                type: 'group',
+                left: 'center',
+                bottom: 10
+            }],
+            series: [{
+                type: 'liquidFill',
+                radius: '80%',
+                center: ['50%', '50%'],
+                data: data,
+                backgroundStyle: {
+                    borderWidth: 0,
+                    borderColor: '#1daaeb',
+                    color: '#00000000'
+                },
+                label: {
+                    normal: {
+                        formatter: function(params){
+                            return two?tilte+'\n'+all+'\n'+(value * 100).toFixed(2) + '%':all+'\n'+'\n'+tilte;
+                        },
+                        textStyle: {
+                            fontSize: 20
+                        }
+                    }
+                }
+            }]
+        }
+
+        this.setState({
+            option:option
+        })
+    }
+    render() {
+        return (
+            <div
+            style={{width:"180px",height:"180px",margin:'10%',background:`url('${ RemoteEchartgd }') 100% 100% / cover no-repeat`}}>
+                <ReactEcharts 
+                 option={this.state.option} 
+                style={{width:"180px",height:"180px"}}
+                  /> 
+            </div>
+        )
+    }
+}
---
+++ src/components/common/LayerEcharts2.js
@@ -0,0 +1,162 @@
+import React, { Component } from 'react'
+import ReactEcharts from 'echarts-for-react';
+import echarts from 'echarts';
+import 'echarts-liquidfill';
+import RemoteEchartgd from '../../style/imgs/RemoteEchartgd.png'
+
+export default class LayerEcharts2 extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            option:{}
+        };
+    }
+    componentWillMount(){
+        var val1data2 = [{
+            value: 0.2,
+            name: '装配完成率',
+        },
+        {
+            value: 0.3,
+            name: '班检完成率',
+        },
+        {
+            value: 0.4,
+            name: '初检完成率',
+        },
+        {
+            value: 0.3,
+            name: '复检完成率',
+        },
+        {
+            value: 0.1,
+            name: '出厂检完成率',
+        },
+    ]
+    
+    var arr = ['middleLost', 0.6, val1data2, '其他']
+    
+    var option = {
+        title: {
+            top: '45%',
+            left: 'center',
+            text: arr[3],
+            textStyle: {
+                color: '#fff',
+                fontStyle: 'normal',
+                fontWeight: 'normal',
+                fontSize: 14
+            },
+            subtext: '占比' + (arr[1] * 10000 / 100).toFixed(2) + '%',
+            subtextStyle: {
+                color: '#fff',
+                fontSize: 12
+            }
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: function(res) {
+                console.log(res)
+    
+                if (res.componentSubType == 'liquidFill') {
+                    return res.seriesName + ': ' + (res.value * 10000 / 100).toFixed(2) + '%';
+                } else {
+                    return '<span class="ii" style="background:' + res.color + ' "></span>' + res.name + ':<br/> ' + res.data.value;
+                }
+            }
+        },
+        series: [{
+                type: 'liquidFill',
+                itemStyle: {
+                    normal: {
+                        opacity: 0.6,
+                        shadowBlur: 0,
+                        shadowColor: 'rgba(0,0,0,0)'
+                    }
+                },
+                name: arr[3],
+                data: [{
+                    value: 0.6,
+                    itemStyle: {
+                        normal: {
+                            color: '#53d5ff',
+                            opacity: 0.6
+                        }
+                    }
+                }],
+              //  background: '#fff',
+                color: ['rgba(0,0,0,0)'],
+                center: ['50%', '50%'],
+              /*  backgroundStyle: {
+                    color: '#fff'
+                },*/
+                label: {
+                    normal: {
+                        formatter: '',
+                        textStyle: {
+                            fontSize: 12
+                        }
+                    }
+                },
+                outline: {
+                    itemStyle: {
+                        borderColor: 'rgba(0,0,0,0)',
+                        borderWidth: 0
+                    },
+                    borderDistance: 0
+                }
+            },
+            {
+                type: 'pie',
+                radius: ['70%', '85%'],
+                     color: ['#b71c1c', '#bf360c','#49dff0', '#034079', '#6f81da', '#00ffb4'],
+                hoverAnimation: false, ////设置饼图默认的展开样式
+                label: {
+                    show: false,
+                    
+                    normal: {
+                        formatter: '{b}\n{d}%',
+                        show: false,
+                        position: 'none'
+                    },
+                },
+                labelLine: {
+                    normal: {
+                        show: false
+                    }
+                },
+
+    
+                itemStyle: { // 此配置
+                    normal: {
+                        borderWidth: 0,
+                        borderColor: '#fff',
+                    },
+                    emphasis: {
+                        borderWidth: 0,
+                        shadowBlur: 2,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                },
+                data: arr[2]
+            }
+        ]
+    }
+        this.setState({
+            option:option
+        })
+
+    }
+    render() {
+        return (
+            <div
+            style={{width:"180px",height:"180px",margin:'10%',background:`url('${ RemoteEchartgd }') 100% 100% / cover no-repeat`}}>
+                <ReactEcharts 
+                 option={this.state.option} 
+                style={{width:"180px",height:"180px"}}
+                  /> 
+            </div>
+        )
+    }
+}
