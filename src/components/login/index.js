8fb48086670b88efee29b9d3a9cac5232a4c160d
diao <1916127652@qq.com>
2019-08-29 20:11:28 +0800

    登录样式的修改

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { Form, Icon, Input, Button, Checkbox,message } from "antd";
 import axios from '../../axios';
+import Earthrolling from './Earthrolling'
 import './index.less';
-import logo from '../../style/imgs/logonew.png'
+import login_logo from '../../style/imgs/login_logo.png'
+import login_xian from '../../style/imgs/login_xian.png'
 import Utils from "../../utils/utils";
+import { isMoment } from 'moment';
 
 const  phoneRexp = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/;
 const FormItem = Form.Item;
@@ -150,19 +153,25 @@ class Login extends Component {
       <div className="Loginx">
           <div className="loginLogo">
               <div className="loginLogo-img">
-                <img src={logo} alt="" />
+                <img className="login_logo_img" src={login_logo} alt="" />
               </div>
           </div>
+          <img className="login_xian" src={login_xian} alt=""/>
           <div className="logincont">
               <div className="login-formnew">
+                  {/* 旋转地球 */}
+                  <div className="mydi">
+                    <Earthrolling />
+                  </div>
+                  {/* 登录框 */}
                   <div className="loginsj">
                       <div className="logindk">
                           <div className="loginformx">
                               <div className="login-type">
-                                  <div className="login-user" style={{ cursor:'pointer' }} onClick={this.useronChage}>
-                                      <span>用户名登录</span>
+                                  <div className="login-user" style={{ cursor:'pointer',color:"#064974" }} onClick={this.useronChage}>
+                                      <span>用户登录</span>
                                   </div>
-                                  <div className="login-authcode" style={{ cursor:'pointer' }} onClick={this.authcodeonChage}>
+                                  <div className="login-authcode" style={{ cursor:'pointer',color:'#000' }} onClick={this.authcodeonChage}>
                                       <span>验证码登录</span>
                                   </div>
                               </div>
@@ -194,17 +203,17 @@ class Login extends Component {
                                               <Form
                                                   onSubmit={this.handleSubmit}
                                                   style={{ maxWidth: "100%", margin: "0 auto" }}>
-                                                  <FormItem>
+                                                  <FormItem style={{width:"300px",margin:"10px 0"}}>
                                                       {getFieldDecorator("userName", {
                                                           rules: [{ required: true, message: "请输入用户名!" }]
                                                       })(
                                                           <Input
                                                               placeholder="请输入用户名"
-                                                              prefix={<Icon type="user" style={{ fontSize: 13 }} />}
+                                                              prefix={<Icon type="user" style={{ fontSize: 13}} />}
                                                           />
                                                       )}
                                                   </FormItem>
-                                                  <FormItem>
+                                                  <FormItem style={{width:"300px",margin:"0 0"}}>
                                                       {getFieldDecorator("passWord", {
                                                           rules: [{ required: true, message: "请输入密码!" }]
                                                       })(
@@ -219,7 +228,7 @@ class Login extends Component {
                                                       {getFieldDecorator("remember", {
                                                           valuePropName: "checked",
                                                           initialValue: true
-                                                      })(<Checkbox className="rember-user">记住用户名</Checkbox>)}
+                                                      })(<Checkbox className="rember-user" style={{color:"#064974"}}>记住用户名</Checkbox>)}
                                                       {/*<span className="login-form-forgot" href="" style={{float: 'right'}}>忘记密码</span>*/}
                                                       <Button
                                                           type="primary"
@@ -236,7 +245,7 @@ class Login extends Component {
                                              <Form
                                                  onSubmit={this.handleSubmitbyAuthcode}
                                                  style={{ maxWidth: "100%", margin: "0 auto" }}>
-                                                 <FormItem>
+                                                 <FormItem style={{width:"300px",margin:"10px 0"}}>
                                                      {getFieldDecorator("phone", {
                                                          rules: [{
                                                              required: true, message: "请输入正确的手机号码!" ,
@@ -250,29 +259,31 @@ class Login extends Component {
                                                      )}
                                                  </FormItem>
                                                  <FormItem
-                                                     style={{ width:"100%" }}
+                                                     style={{width:"100%",margin:"0 0"}}
                                                  >
-                                                     {getFieldDecorator("authCode", {
-                                                         // rules: [{ required: true, message: "请输入短信验证码!" }]
-                                                     })(
-                                                         <Input
-                                                             placeholder="请输入短信验证码"
-                                                             prefix={<Icon type="lock" style={{ fontSize: 13 }} />}
-                                                             type="password"
-                                                         />
-
-                                                     )}
-                                                         {
-                                                             !this.state.isGetCode?
-                                                                 <div className="getAuthCode" style={{ cursor:'pointer' }} onClick={this.getPhoneCode}>{ this.state.btnTxt }</div>:
-                                                                 <div className="timeer">{this.state.countDown}s</div>
-                                                         }
+                                                     <div className="myflex">
+                                                        {getFieldDecorator("authCode", {
+                                                            // rules: [{ required: true, message: "请输入短信验证码!" }]
+                                                        })(
+                                                            <Input
+                                                                style={{width:"70%"}}
+                                                                placeholder="输入验证码"
+                                                                prefix={<Icon type="lock" style={{ fontSize: 13 }} />}
+                                                                type="password"
+                                                            />
+                                                        )}
+                                                            {
+                                                                !this.state.isGetCode?
+                                                                    <div className="getAuthCode" style={{ cursor:'pointer',width:"30%" }} onClick={this.getPhoneCode}>{ this.state.btnTxt }</div>:
+                                                                    <div className="timeer" style={{ cursor:'pointer',width:"30%" }}>{this.state.countDown}</div>
+                                                            }
+                                                    </div>
                                                  </FormItem>
                                                  <FormItem>
                                                      {getFieldDecorator("remember", {
                                                          valuePropName: "checked",
                                                          initialValue: true
-                                                     })(<Checkbox className="rember-user">记住用户名</Checkbox>)}
+                                                     })(<Checkbox className="rember-user" style={{color:"#064974"}}>记住用户名</Checkbox>)}
                                                      {/*<span className="login-form-forgot" href="" style={{float: 'right'}}>忘记密码</span>*/}
                                                      <Button
                                                          type="primary"
@@ -290,6 +301,8 @@ class Login extends Component {
                           </div>
                       </div>
                   </div>
+                  
+                  
               </div>
           </div>
           <span className="BA-ICP"> 陕ICP备19013652号-1</span>
